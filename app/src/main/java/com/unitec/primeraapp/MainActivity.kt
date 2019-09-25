f45dc392b58c4c3741fb1ff3832741e803a1512b
ivanluj27 <aliven_pz@outlook.com>
2019-09-24 19:42:18 -0500

    Clase 2

--- app/src/main/java/com/unitec/primeraapp/MainActivity.kt
+++ app/src/main/java/com/unitec/primeraapp/MainActivity.kt
@@ -8,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //quitamos la action bar que aparece de defecto
+        actionBar?.hide()
+
     }
 }
+
+
